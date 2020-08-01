e0c29c575e188b2a298201527b5bd86b48925487
tolgacakn <64797627+tolgacakn@users.noreply.github.com>
2020-08-01 14:13:21 +0300

    Update rps.js

--- rps.js
+++ rps.js
@@ -1,61 +1,81 @@
 class RockPaperScissors {
-  constructor(User) {
-    this.class = User.className;
-  }
+	constructor(User) {
+		this.class = User.className;
+		computerPoints = 0;
+		playerPoints = 0;
+	}
 
-  execute() {
-    this.getClass();
-    this.winning();
-    console.log(`class is ${this.class}`);
-    console.log(`answer is ${this.answer}`);
-    answer.innerHTML = this.answer;
-    output.innerHTML = this.winning();
-  }
+	execute() {
+		this.getClass();
+		this.winning();
+		answer.innerHTML = this.answer;
+		output.innerHTML = this.winning();
+		this.updateScoreBoard();
+	}
 
-  changeAnswer() {
-    const random = Math.round(Math.random() * 2);
-    const answers = ['rock', 'paper', 'scissors'];
-    this.answer = answers[random];
-    return this.answer;
-  }
+	changeAnswer() {
+		const random = Math.round(Math.random() * 2);
+		const answers = ['rock', 'paper', 'scissors'];
+		this.answer = answers[random];
+		return this.answer;
+	}
 
-  getClass() {
-    this.changeAnswer();
-    return this.class;
-  }
-  winning() {
-    if (this.class == this.answer) {
-      return 'its a tie';
-    } else if (this.class == 'rock' && this.answer == 'scissors') {
-      return `${this.class} beats ${this.answer}, YOU WON`;
-    } else if (this.class == 'scissors' && this.answer == 'rock') {
-      return `${this.answer} beats ${this.class}, YOU LOST`;
-    } else if (this.class == 'scissors' && this.answer == 'paper') {
-      return `${this.class} beats ${this.answer}, YOU WON`;
-    } else if (this.class == 'paper' && this.answer == 'scissors') {
-      return `${this.answer} beats ${this.class}, YOU LOST`;
-    } else if (this.class == 'paper' && this.answer == 'rock') {
-      return `${this.class} beats ${this.answer}, YOU WON`;
-    } else if (this.class == 'rock' && this.answer == 'paper') {
-      return `${this.answer} beats ${this.class}, YOU LOST`;
-    }
-  }
+	getClass() {
+		this.changeAnswer();
+		return this.class;
+	}
+	winning() {
+		if (this.class == this.answer) {
+			return 'its a tie';
+		} else if (this.class == 'rock' && this.answer == 'scissors') {
+			playerPoints++;
+			return `${this.class} beats ${this.answer}, YOU WON`;
+		} else if (this.class == 'scissors' && this.answer == 'rock') {
+			computerPoints++;
+			return `${this.answer} beats ${this.class}, YOU LOST`;
+		} else if (this.class == 'scissors' && this.answer == 'paper') {
+			playerPoints++;
+			return `${this.class} beats ${this.answer}, YOU WON`;
+		} else if (this.class == 'paper' && this.answer == 'scissors') {
+			computerPoints++;
+			return `${this.answer} beats ${this.class}, YOU LOST`;
+		} else if (this.class == 'paper' && this.answer == 'rock') {
+			playerPoints++;
+			return `${this.class} beats ${this.answer}, YOU WON`;
+		} else if (this.class == 'rock' && this.answer == 'paper') {
+			computerPoints++;
+			return `${this.answer} beats ${this.class}, YOU LOST`;
+		}
+	}
+	updateScoreBoard() {
+		point.innerHTML = `${computerPoints / 2} - ${playerPoints / 2}`;
+	}
 }
+
 function upperText() {
-  setTimeout(function () {
-    x.innerHTML = 'ROCK';
-  }, 300);
-  setTimeout(function () {
-    y.innerHTML = 'PAPER';
-  }, 600);
-  setTimeout(function () {
-    z.innerHTML = 'SCISSORS';
-  }, 900);
-  setTimeout(function () {
-    shoot.innerHTML = 'SHOOT';
-  }, 1200);
-}
+	answer.innerHTML = '';
+	output.innerHTML = '';
+	x.innerHTML = '';
+	y.innerHTML = '';
+	z.innerHTML = '';
+	shoot.innerHTML = '';
 
+	setTimeout(function () {
+		x.innerHTML = 'ROCK';
+	}, 300);
+
+	setTimeout(function () {
+		y.innerHTML = 'PAPER';
+	}, 600);
+
+	setTimeout(function () {
+		z.innerHTML = 'SCISSORS';
+	}, 900);
+
+	setTimeout(function () {
+		shoot.innerHTML = 'SHOOT';
+	}, 1200);
+}
 var answer = document.querySelector('h2');
 var rock = document.querySelector('.rock');
 var paper = document.querySelector('.paper');
@@ -68,22 +88,29 @@ var shoot = document.querySelector('.shoot');
 const Rock = new RockPaperScissors(rock);
 const Paper = new RockPaperScissors(paper);
 const Scissors = new RockPaperScissors(scissors);
+var point = document.querySelector('.points');
+var computerPoints = 0;
+var playerPoints = 0;
+const reset = document.querySelector('.reset');
 
 rock.addEventListener('click', function () {
-  upperText();
-  setTimeout(function () {
-    Rock.execute();
-  }, 1700);
+	upperText();
+	setTimeout(function () {
+		Rock.execute();
+	}, 1700);
 });
 paper.addEventListener('click', function () {
-  upperText();
-  setTimeout(function () {
-    Paper.execute();
-  }, 1700);
+	upperText();
+	setTimeout(function () {
+		Paper.execute();
+	}, 1700);
 });
 scissors.addEventListener('click', function () {
-  upperText();
-  setTimeout(function () {
-    Scissors.execute();
-  }, 1700);
+	upperText();
+	setTimeout(function () {
+		Scissors.execute();
+	}, 1700);
+});
+reset.addEventListener('click', () => {
+	location.reload();
 });
